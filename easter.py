2e1c60685b86fa6940cfcc910f084207b4b85007
EricShay <erichsueh1337@hotmail.com>
2018-03-27 16:15:29 -0600

    added code

---
+++ easter.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+import roslib
+import rospy
+import smach
+import smach_ros
+from geometry_msgs.msg import Twist
+from sensor_msgs.msg import LaserScan
+import numpy as np
+import random
+import math
+from sensor_msgs.msg import Joy
+from sound_play.msg import SoundRequest
+from sound_play.libsoundplay import SoundClient
+
+class Localization(smach.State):
+    def __init__(self):
+        smach.State.__init__(self, outcomes=['Localization','Exploration','Found','Sound'])
+
+    def execute(self, userdata):
+        
+        #do stuff here
+
+class Exploration(smach.State):
+    def __init__(self):
+        smach.State.__init__(self, outcomes['Localization','Exploration','Found','Sound'])
+
+    def execute(self, userdata):
+        
+        #do stuff here
+
+class Found(smach.State):
+    def __init__(self):
+        smach.State.__init__(self, outcomes['Localization','Exploration','Found','Sound'])
+
+    def execute(self, userdata):
+
+        #do stuff here
+
+class Sound(smach.State):
+    def __init__(self):
+        smach.State.__init__(self, outcomes['Localization','Exploration','Found','Sound'])
+
+    def execute(self, userdata):
+
+        #do stuff here
+
+
+def main():
+    rospy.init_node('Egghunt')
+
+    sm = smach.StateMachine(outcomes=[])
+
+    with sm:
+        smach.StateMachine.add('Localization', Localization(), transitions = {'Localization':'Localization','Exploration':'Exploration'})
+        smach.StateMachine.add('Exploration', Exploration(), transitions = {'Localization':'Localization', 'Exploration':'Exploration','Found':'Found'})
+        smach.StateMachine.add('Found', Found(), transitions = {'Localization':'Localization', 'Exploration':'Exploration','Found':'Found','Sound':'Sound'})
+        smach.StateMachine.add('Sound', Sound(), transitions = {'Sound':'Sound','Exploration':'Exploration'})
+
+    sis = smach_ros.IntrospectionServer('Egghunt', sm, '/SM_ROOT')
+    sis.start()
+    
+    outcome = sm.execute()
+    sis.stop()
+
+if __name__ == '__main__':
+    main()
